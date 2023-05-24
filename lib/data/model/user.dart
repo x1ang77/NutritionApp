9512a51a3b442b74a624c724f1b2e8669974b656
juju <jcmc888@gmail.com>
2023-05-24 14:21:42 +0800

    fixed register

--- lib/data/model/user.dart
+++ lib/data/model/user.dart
@@ -8,12 +8,15 @@ class User {
   final String email;
   final String password;
   final String? image;
-  final String gender;
-  final int age;
-  final double height;
-  final double weight;
+  final String? gender;
+  final int? age;
+  final double? height;
+  final double? weight;
   final String? diet;
-  final double? calorieIntake;
+  final double? calorieGoal;
+  final double? carbGoal;
+  final double? proteinGoal;
+  final double? fatGoal;
 
   static const String tableName = "users";
 
@@ -23,13 +26,24 @@ class User {
     required this.email,
     required this.password,
     this.image,
-    this.gender = '',
-    this.age = 0,
-    this.height = 0.0,
-    this.weight = 0.0,
-    this.diet = '',
-    this.calorieIntake = 0.0,
+    this.gender,
+    this.age,
+    this.height,
+    this.weight,
+    this.diet,
+    this.calorieGoal = 2000,
+    this.carbGoal = 600,
+    this.proteinGoal = 200,
+    this.fatGoal = 100,
   });
+
+  User copyWith({String? username, String? email, String? password,}) {
+    return User(
+      username: username ?? this.username,
+      email: email ?? this.email,
+      password: password ?? this.password,
+    );
+  }
 
   Map<String, dynamic> toMap() {
     final String hashedPassword = md5.convert(utf8.encode(password)).toString();
@@ -44,7 +58,10 @@ class User {
       "height": height,
       "weight": weight,
       "diet": diet,
-      "calorieIntake": calorieIntake
+      "calorie_goal": calorieGoal ?? 2000,
+      "carb_goal": carbGoal ?? 600,
+      "protein_goal": proteinGoal ?? 200,
+      "fat_goal": fatGoal ?? 100
     };
   }
 
@@ -55,12 +72,15 @@ class User {
       email: map["email"],
       password: map["password"],
       image: map["image"],
-      gender: map["gender"] ?? "",
+      gender: map["gender"],
       age: map["age"] ?? 0,
       height: map["height"] ?? 0.0,
       weight: map["weight"] ?? 0.0,
       diet: map["diet"] ?? '',
-      calorieIntake: map["calorieIntake"] ?? 0.0,
+      calorieGoal: map["calorie_goal"] ?? 0.0,
+      carbGoal: map["carb_goal"] ?? 0.0,
+      proteinGoal: map["protein_goal"] ?? 0.0,
+      fatGoal: map["fat_goal"] ?? 0.0
     );
   }
 }
